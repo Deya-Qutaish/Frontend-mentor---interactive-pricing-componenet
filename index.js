cf21cc3272b9af04fa7f537a4d6250cb79218a73
Deya Qutaish <53603755+Deya-Qutaish@users.noreply.github.com>
2023-02-24 06:31:02 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,46 @@
 "use strict";
 
-const slider = document.querySelector(".range");
-const check = document.querySelector(".check");
+const checkBox = document.querySelector(".check");
 const price = document.querySelector(".price");
 const time = document.querySelector(".time");
+const sliderInput = document.querySelector(".range");
+const views = document.querySelector(".views");
 
-check.addEventListener("click", () => {
-  if (check.checked) {
+let priceValue = 16;
+
+sliderInput.addEventListener("input", () => {
+  const value = sliderInput.value;
+
+  if (value === "3") {
+    priceValue = 16;
+    views.textContent = " 100K PAGEVIEWS";
+  } else if (value === "2") {
+    priceValue = 12;
+    views.textContent = " 50K PAGEVIEWS";
+  } else if (value === "1") {
+    priceValue = 8;
+    views.textContent = " 10K PAGEVIEWS";
+  } else if (value === "4") {
+    priceValue = 24;
+    views.textContent = " 500K PAGEVIEWS";
+  } else if (value === "5") {
+    priceValue = 36;
+    views.textContent = " 1M PAGEVIEWS";
+  }
+
+  if (!checkBox.checked) {
+    price.textContent = `$${priceValue.toFixed(2)}`;
+  } else {
+    price.textContent = `$${priceValue.toFixed(2) * 12 * 0.75}`;
+  }
+});
+
+checkBox.addEventListener("click", () => {
+  if (checkBox.checked) {
     time.textContent = "/year";
-    price.textContent = "$144.00";
+    price.textContent = `$${priceValue.toFixed(2) * 12 * 0.75}`;
   } else {
     time.textContent = "/month";
-    price.textContent = "$16.00";
+    price.textContent = `$${priceValue.toFixed(2)}`;
   }
 });
